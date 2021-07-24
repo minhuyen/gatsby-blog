6615b6c4fb3289c2a153a4d7547411b958ffc03e
Uyen Do <minhuyendo@gmail.com>
2021-07-24 21:27:11 +0700

    Preparing a Site to go live

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Blog",
+    title: `Pandas Eating Lots`,
+    description: `A simple description about pandas eating lots...`,
+    author: `gatsbyjs`,
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
@@ -11,9 +13,25 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/src/blog/`,
+        path: `${__dirname}/content/blog/`,
       },
     },
     "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
   ],
 };
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,11 +2,13 @@ import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const BlogPost = ({ data }) => {
   const post = data.mdx;
   return (
     <Layout>
+      <Seo title={post.frontmatter.title} description={post.excerpt} />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <p>Posted: {post.frontmatter.date}</p>
@@ -25,6 +27,7 @@ export const query = graphql`
       }
       id
       body
+      excerpt
     }
   }
 `;
